469c89ae496526da5b2a30eb14fcf64cd1fa7732
Apelllsinka <apelllsinka24@gmail.com>
2025-01-16 19:34:02 +0300

    Update MainWindow.py
    added choice Gamemodes in 'Start Game'

--- MainWindow.py
+++ MainWindow.py
@@ -1,9 +1,7 @@
 import pygame
 import os
 
-clicked = 0
-
-
+Gamemode = 2
 def BasicGamemodes(e, m):  # просто вовзаращаю базовое меню режимов игры
     pygame.draw.rect(e, pygame.Color('White'), (200, 100, 400, 60))
     text_surface = m.render('Выживание', False, (0, 0, 0))
@@ -18,8 +16,9 @@ def BasicGamemodes(e, m):  # просто вовзаращаю базовое м
     e.blit(text_surface2, (335, 312))
 
 
-def Gamemodes():
-    global clicked
+def Gamemodes(k):
+    global Gamemode
+    clicked = 1
     pygame.init()
     pygame.font.init()
     screen1 = pygame.display.set_mode((800, 800))
@@ -28,12 +27,12 @@ def Gamemodes():
     text_surface = my_font.render('Start Game!', False, (0, 0, 0))
     text_surface1 = my_font.render('Gamemode', False, (0, 0, 0))
     text_surface2 = my_font.render('Settings', False, (0, 0, 0))
-    pygame.draw.rect(screen1, pygame.Color('White'), (200, 100, 400, 60))
-    screen1.blit(text_surface, (315, 112))
     pygame.draw.rect(screen1, pygame.Color('White'), (200, 200, 400, 60))
-    screen1.blit(text_surface1, (325, 212))
+    screen1.blit(text_surface, (315, 212))
     pygame.draw.rect(screen1, pygame.Color('White'), (200, 300, 400, 60))
-    screen1.blit(text_surface2, (335, 312))
+    screen1.blit(text_surface1, (325, 312))
+    pygame.draw.rect(screen1, pygame.Color('White'), (200, 400, 400, 60))
+    screen1.blit(text_surface2, (335, 412))
     Y = False
     while not Y:
         for event in pygame.event.get():
@@ -41,47 +40,53 @@ def Gamemodes():
                 pygame.quit()
                 mainWindow()
             x, y = map(int, pygame.mouse.get_pos())
-            if 100 <= y <= 160 and 200 <= x <= 600:
-                pygame.draw.rect(screen1, pygame.Color('Black'), (200, 100, 400, 60))
-                text_surface = my_font.render('Выживание', False, pygame.Color('White'))
-                screen1.blit(text_surface, (315, 112))
-            else:
-                pygame.draw.rect(screen1, pygame.Color('White'), (200, 100, 400, 60))
-                text_surface = my_font.render('Выживание', False, (0, 0, 0))
-                screen1.blit(text_surface, (315, 112))
             if 200 <= y <= 260 and 200 <= x <= 600:
                 pygame.draw.rect(screen1, pygame.Color('Black'), (200, 200, 400, 60))
-                text_surface1 = my_font.render('Оригинальный', False, pygame.Color('White'))
-                screen1.blit(text_surface1, (325, 212))
+                text_surface = my_font.render('Выживание', False, pygame.Color('White'))
+                screen1.blit(text_surface, (315, 212))
             else:
                 pygame.draw.rect(screen1, pygame.Color('White'), (200, 200, 400, 60))
-                text_surface1 = my_font.render('Оригинальный', False, (0, 0, 0))
-                screen1.blit(text_surface1, (325, 212))
+                text_surface = my_font.render('Выживание', False, (0, 0, 0))
+                screen1.blit(text_surface, (315, 212))
             if 300 <= y <= 360 and 200 <= x <= 600:
                 pygame.draw.rect(screen1, pygame.Color('Black'), (200, 300, 400, 60))
-                text_surface2 = my_font.render('Пазл', False, pygame.Color('White'))
-                screen1.blit(text_surface2, (335, 312))
+                text_surface1 = my_font.render('Оригинальный', False, pygame.Color('White'))
+                screen1.blit(text_surface1, (325, 312))
             else:
                 pygame.draw.rect(screen1, pygame.Color('White'), (200, 300, 400, 60))
+                text_surface1 = my_font.render('Оригинальный', False, (0, 0, 0))
+                screen1.blit(text_surface1, (325, 312))
+            if 400 <= y <= 460 and 200 <= x <= 600:
+                pygame.draw.rect(screen1, pygame.Color('Black'), (200, 400, 400, 60))
+                text_surface2 = my_font.render('Пазл', False, pygame.Color('White'))
+                screen1.blit(text_surface2, (335, 412))
+            else:
+                pygame.draw.rect(screen1, pygame.Color('White'), (200, 400, 400, 60))
                 text_surface2 = my_font.render('Пазл', False, (0, 0, 0))
-                screen1.blit(text_surface2, (335, 312))
-            if 200 <= x <= 600 and event.type == pygame.MOUSEBUTTONDOWN:
-                if 100 <= y <= 160:
-                    clicked = 1
-                    pygame.draw.rect(screen1, pygame.Color('Black'), (200, 100, 400, 60))
-                    text_surface = my_font.render('Сохранено!', False, pygame.Color('White'))
-                    screen1.blit(text_surface, (315, 112))
-                elif 200 <= y <= 260:
-                    clicked = 2
-                    pygame.draw.rect(screen1, pygame.Color('Black'), (200, 200, 400, 60))
-                    text_surface1 = my_font.render('Сохранено!', False, pygame.Color('White'))
-                    screen1.blit(text_surface1, (325, 212))
+                screen1.blit(text_surface2, (335, 412))
+            if 200 <= x <= 600 and event.type == pygame.MOUSEBUTTONUP and clicked == 0:
+                if 200 <= y <= 260:
+                    Gamemode = 1
+                    if k == 2:
+                        pygame.quit()
+                        mainWindow()
+                    return
                 elif 300 <= y <= 360:
-                    clicked = 3
-                    pygame.draw.rect(screen1, pygame.Color('Black'), (200, 300, 400, 60))
-                    text_surface2 = my_font.render('Сохранено', False, pygame.Color('White'))
-                    screen1.blit(text_surface2, (335, 312))
+                    Gamemode = 2
+                    if k == 2:
+                        pygame.quit()
+                        mainWindow()
+                    return
+                elif 400 <= y <= 460:
+                    Gamemode = 3
+                    if k == 2:
+                        pygame.quit()
+                        mainWindow()
+                    return
+            if event.type == pygame.MOUSEBUTTONUP:
+                clicked = 0
         pygame.display.flip()
+
 
 def mainWindow():
     pygame.init()
@@ -128,13 +133,19 @@ def mainWindow():
                 screen.blit(text_surface2, (335, 312))
             if 200 <= x <= 600 and event.type == pygame.MOUSEBUTTONDOWN:
                 if 100 <= y <= 160:
+                    Gamemodes(1)
+                    print(Gamemode)
                     os.system('main.py')
+                    exit()
                 elif 200 <= y <= 260:
-                    Gamemodes()
+                    Gamemodes(2)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
         pygame.display.flip()
+
+
+mainWindow()
 
 
 mainWindow()
